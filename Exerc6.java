99bc42390962ada53cfe644bdb2108980fa35eb5
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-03-28 19:53:06 +0000

    Semana6

--- Exerc6.java
+++ Exerc6.java
@@ -13,9 +13,11 @@ public class Exerc6 {
                     somaSala1+=sala1[i][j]; }
                     else{
                         somaSala2+=sala1[i][j];
-                    }
-
+                        }
             }
+            sc.close();
+            System.out.println("Asoma das idades da sala1: "+somaSala1);
+            System.out.println("Asoma das idades da sala1: "+somaSala2);
         }
     }
 }
